6556403bf6f9359690bd76bec196535d623ee7b8
Jan Kundrát <jkt@jankundrat.com>
2025-05-14 21:41:18 +0200

    fix a missing f-string
    Fixes: c72374c ("Update the async Tridonic DALI-USB driver to work with 24-bit frames")

--- dali/driver/hid.py
+++ dali/driver/hid.py
@@ -447,7 +447,7 @@ class tridonic(hid):
             response = None
             while outstanding_transmissions or response is None:
                 self._log.debug(f"waiting for {outstanding_transmissions=} "
-                                "{response=}")
+                                f"{response=}")
                 if len(messages) == 0:
                     await event.wait()
                     event.clear()
